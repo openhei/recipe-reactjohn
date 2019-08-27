0132104682645a55d222be57925725646524081d
Chris Yi <chris@Chris.local>
2019-08-27 16:11:32 -0400

    add search component

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react'
+import RecipeList from '../components/RecipeList'
+import Search from '../components/Search'
+import { recipeData } from '../data/tempList'
 
 export default class Recipes extends Component {
+  constructor(props) {
+    super(props)
+  }
+  state = {
+    recipes: recipeData,
+    search: ''
+  }
+  handleChange = event => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+  }
   render() {
-    return <h4>hello from recipe</h4>
+    return (
+      <>
+        <Search
+          search={this.state.search}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <RecipeList recipes={this.state.recipes} />
+      </>
+    )
   }
 }
